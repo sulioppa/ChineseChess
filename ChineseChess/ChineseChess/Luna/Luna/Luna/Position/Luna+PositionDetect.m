f0df75779acb3192d172fbf143b8d2dd4ab51d7a
夙璃 <sulioppa@icloud.com>
2019-01-28 17:14:52 +0800

    Fix Memory Crash（修复Position hash崩溃）.

--- ChineseChess/ChineseChess/Luna/Luna/Luna/Position/Luna+PositionDetect.m
+++ ChineseChess/ChineseChess/Luna/Luna/Luna/Position/Luna+PositionDetect.m
@@ -15,7 +15,7 @@ const LCZobristHash LCPositionHashHighMask = 0x1ff00; // 0001 1111 1111 0000 000
 const LCZobristKey LCPositionHashLowMask = 0xff; // 1111 1111.
 
 LCMutablePositionHashRef LCPositionHashCreateMutable(void) {
-    const UInt64 size = sizeof(LCZobristHash) * (1 << 18);
+    const UInt64 size = sizeof(LCPositionHash) * (1 << 18);
     
     void *memory = malloc(size);
     
@@ -23,7 +23,7 @@ LCMutablePositionHashRef LCPositionHashCreateMutable(void) {
 }
 
 void LCPositionHashClear(LCMutablePositionHashRef hash) {
-    const UInt64 size = sizeof(LCZobristHash) * (1 << 18);
+    const UInt64 size = sizeof(LCPositionHash) * (1 << 18);
     
     memset(hash, 0, size);
 }
