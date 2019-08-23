1be7fd4324c3b15258cc61f7918bcd3f67bb004a
李夙璃 <lisuli@bytedance.com>
2019-08-23 17:08:52 +0800

    become a function（抽取函数）

--- ChineseChess/ChineseChess/ViewController/BaseVC/BaseViewController.swift
+++ ChineseChess/ChineseChess/ViewController/BaseVC/BaseViewController.swift
@@ -9,14 +9,19 @@
 import UIKit
 
 class BaseViewController: UIViewController {
-    
-    init() {
-        super.init(nibName: nil, bundle: nil)
-        self.modalPresentationStyle = .fullScreen
+        
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        self.initAppearance()
     }
     
     required init?(coder: NSCoder) {
         super.init(nibName: nil, bundle: nil)
+        self.initAppearance()
+    }
+    
+    private func initAppearance() {
+        self.modalPresentationStyle = .fullScreen
     }
     
     // MARK: - Status Bar
